16ae7bff93b5d2942a98ac68b1720d332ec5e0fd
leadingcoder0 <59894207+leadingcoder0@users.noreply.github.com>
2022-04-04 00:37:10 -0400

    Update IntakeDown.java
    Sets intake to go down and times out for 5 seconds, forgot to make commit comment for IntakeUp but its the same thing but up

--- GhettoBot/src/main/java/frc/robot/commands/SeqCommands/IntakeDown.java
+++ GhettoBot/src/main/java/frc/robot/commands/SeqCommands/IntakeDown.java
@@ -11,6 +11,7 @@ import frc.robot.subsystems.IntakeSubsystem;
 public class IntakeDown extends CommandBase {
 
     private final IntakeSubsystem m_IntakeSubsystem;
+    private final double intakeStartTime;
 
     public IntakeDown(IntakeSubsystem iSubsystem ) {
 
@@ -22,24 +23,31 @@ public class IntakeDown extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        m_IntakeSubsystem.liftDown(.5);
+        intakeStartTime = Timer.getFPGATimestamp()
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-       
+        m_IntakeSubsystem.liftDown(.5);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        m_intakeSubsystem.holdLiftPos();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return true;
+        if (Timer.getFPGATimestamp() - intakeStartTime > 5) { //Stops after 5 seconds
+            return true;
+        }
+        if (m_intakeSubsystem.atLowerLimit) { //Stops once reaches upper limit
+            return true;
+        }
+        return false;
     }
 
     @Override
