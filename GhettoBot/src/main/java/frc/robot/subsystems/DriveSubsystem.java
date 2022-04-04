4cc23fa4f7095b4c72169fa313ccd01591fb0588
leadingcoder0 <59894207+leadingcoder0@users.noreply.github.com>
2022-04-03 23:28:25 -0400

    Update DriveSubsystem.java
    returns angle from -180 to 180 in getHeading, originally was returning the angle -infinity to infinity

--- GhettoBot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ GhettoBot/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -175,7 +175,7 @@ leftMotorControllerLeader.getEncoder().setPosition(0);
     }
 
     public double getHeading() {
-        return ahrs.getAngle(); 
+        return Math.IEEEremainder(ahrs.getAngle(), 360);
     }
 
     public void resetAngle() {
